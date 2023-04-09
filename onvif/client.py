6e08e341a744f5f3a7b96bfe0e453f581532e820
J. Nick Koston <nick@koston.org>
2023-04-09 10:17:34 -1000

    Allow older version of ssl and older ciphers
    related issue https://github.com/home-assistant/core/issues/91130

--- onvif/client.py
+++ onvif/client.py
@@ -30,14 +30,15 @@ logging.getLogger("zeep.client").setLevel(logging.CRITICAL)
 def create_no_verify_ssl_context() -> ssl.SSLContext:
     """Return an SSL context that does not verify the server certificate.
     This is a copy of aiohttp's create_default_context() function, with the
-    ssl verify turned off.
+    ssl verify turned off and old SSL versions enabled.
+
     https://github.com/aio-libs/aiohttp/blob/33953f110e97eecc707e1402daa8d543f38a189b/aiohttp/connector.py#L911
     """
     sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    sslcontext.options |= ssl.OP_NO_SSLv2
-    sslcontext.options |= ssl.OP_NO_SSLv3
     sslcontext.check_hostname = False
     sslcontext.verify_mode = ssl.CERT_NONE
+    # Allow all ciphers rather than only Python 3.10 default
+    sslcontext.set_ciphers("DEFAULT")
     with contextlib.suppress(AttributeError):
         # This only works for OpenSSL >= 1.0.0
         sslcontext.options |= ssl.OP_NO_COMPRESSION
