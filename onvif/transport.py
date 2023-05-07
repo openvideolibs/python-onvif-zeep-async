dd82b8fc8a67996c3bdc8cf73b50298800f1aca0
J. Nick Koston <nick@koston.org>
2023-05-06 22:11:42 -0500

    it was already fixed

--- onvif/transport.py
+++ onvif/transport.py
@@ -15,12 +15,6 @@ class AsyncSafeTransport(Transport):
         """Load the given XML document."""
         if not path_isfile(url):
             raise RuntimeError(f"Loading {url} is not supported in async mode")
-        # Ideally this would happen in the executor but the library
-        # does not call this from a coroutine so the best we can do
-        # without a major refactor is to cache this so it only happens
-        # once per process at startup. Previously it would happen once
-        # per service per camera per setup which is a lot of blocking
-        # I/O in the event loop so this is a major improvement.
         with open(os.path.expanduser(url), "rb") as fh:
             return fh.read()
 
