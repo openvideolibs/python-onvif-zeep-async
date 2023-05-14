765e60e70b540e6ba4f4d3cff92596d40dab847a
J. Nick Koston <nick@koston.org>
2023-05-14 10:16:44 -0500

    fix requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ here = os.path.abspath(os.path.dirname(__file__))
 version_path = os.path.join(here, "onvif/version.txt")
 version = open(version_path).read().strip()
 
-requires = ["httpx>=0.19.0,<1.0.0", "zeep[async]>=4.2.1,<5.0.0"]
+requires = [
+    "httpx>=0.19.0,<1.0.0",
+    "zeep[async]>=4.2.1,<5.0.0",
+    "ciso8601>=2.1.3",
+    "isodate>=0.6.0",
+]
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
