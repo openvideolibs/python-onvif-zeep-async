2c2f32f92f1103f33c6ae111bd4e26f9f83a14ec
J. Nick Koston <nick@koston.org>
2025-01-22 14:26:16 -1000

    add cover

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,6 +3,7 @@ from onvif.client import strip_user_pass_url, obscure_user_pass_url
 
 def test_strip_user_pass_url():
     assert strip_user_pass_url("http://1.2.3.4/?user=foo&pass=bar") == "http://1.2.3.4/"
+    assert strip_user_pass_url("http://1.2.3.4/") == "http://1.2.3.4/"
 
 
 def test_obscure_user_pass_url():
@@ -10,3 +11,4 @@ def test_obscure_user_pass_url():
         obscure_user_pass_url("http://1.2.3.4/?user=foo&pass=bar")
         == "http://1.2.3.4/?user=********&pass=********"
     )
+    assert obscure_user_pass_url("http://1.2.3.4/") == "http://1.2.3.4/"
